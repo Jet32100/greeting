85e1cc0c5cd74a7933b396ec25c1a478cf086ded
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-23 12:22:11 -0600

    mostly completed greeting challenges

---
+++ code challenge/greeting.rb
@@ -0,0 +1,34 @@
+#Create a simple greeting app that you can interact with through your terminal.
+	# A user will enter their name and see a greeting returned that uses the name they entered
+
+
+
+############# STEP ONE: Accept user input ######################
+#Print Instructions to Terminal that instruct the user(person running the program) to enter their name
+
+
+#Accept user input using the gets method
+	#Store that user input in a variable
+	# .chomp the user input so that it doesn't have a trailing line break
+	#the method returns a string that looks something like this:  “Top of the Morning to you James!”
+
+
+
+
+
+
+########## STEP TWO: Define your method #########################
+
+#Create a new method ( with a semantic name )
+	#While defining the method, set a parameter so that it will accept an argument when we later call the method
+	#Create a greeting and interpolate or concatenate the name parameter
+	#Remember to end your method!
+
+
+
+
+
+###### STEP THREE: Call your method ##########
+	#Call the method you just created above
+	#be sure to pass a name arguement
+	#The argument you pass should be the variable you set with the user input 
